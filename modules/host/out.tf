b7c6f461a8405f7450ed6410a780c2787772c97a
Gabor Simko <gsimko3@gmail.com>
2024-04-22 00:08:29 -0700

    fix

--- modules/host/out.tf
+++ modules/host/out.tf
@@ -7,7 +7,7 @@ output "ipv6_address" {
 }
 
 output "private_ipv4_address" {
-  value = hcloud_server_network.server[0].ip
+  value = var.use_private_network ? hcloud_server_network.server[0].ip : null
 }
 
 output "name" {
