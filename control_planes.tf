ac2b4872ba96678fe99a074c978e62b5cd7ff4f1
Gabor Simko <gsimko3@gmail.com>
2024-04-24 00:56:16 -0700

    fix

--- control_planes.tf
+++ control_planes.tf
@@ -105,8 +105,8 @@ locals {
       kubelet-arg                 = concat(local.kubelet_arg, var.k3s_global_kubelet_args, var.k3s_control_plane_kubelet_args, v.kubelet_args)
       kube-controller-manager-arg = local.kube_controller_manager_arg
       flannel-iface               = local.flannel_iface
-      node-ip                     = var.use_private_network ? module.control_planes[k].private_ipv4_address : module.control_planes[k].ipv4_address
-      advertise-address           = var.use_private_network ? module.control_planes[k].private_ipv4_address : module.control_planes[k].ipv4_address
+      node-ip                     = module.control_planes[k].private_ipv4_address
+      advertise-address           = module.control_planes[k].private_ipv4_address
       node-label                  = v.labels
       node-taint                  = v.taints
       selinux                     = true
